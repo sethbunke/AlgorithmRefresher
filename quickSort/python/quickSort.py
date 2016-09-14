b257d1a9792caeadf5cbfe4fe30ce705f437cc91
Seth Bunke <seth.bunke@hotmail.com>
2016-09-14 22:54:48 +0000

    comment added

--- quickSort/python/quickSort.py
+++ quickSort/python/quickSort.py
@@ -1,9 +1,13 @@
-
+from random import randint
+#O(n log n) on average; O(n^2) in the worst case
+#In averate there are O(log n) levels (split the array) and 
+#each level takes O(n) (touch each element in array)
+#Worst case ()
 def quick_sort(arr):
     if len(arr) < 2:
         return arr
     else:
-        pivot = arr[0]
+        pivot = arr[0] #this needs to be random
         less = [i for i in arr[1:] if i <= pivot]
         greater = [i for i in arr[1:] if i > pivot]
         return quick_sort(less) + [pivot] + quick_sort(greater)
