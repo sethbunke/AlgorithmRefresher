dabf68392ab0b6c4d1fb9bb45759218092a39bb2
Seth Bunke <seth.bunke@hotmail.com>
2016-09-14 22:35:30 +0000

    added quickstort

---
+++ quickSort/python/quickSort.py
@@ -0,0 +1,11 @@
+
+def quick_sort(arr):
+    if len(arr) < 2:
+        return arr
+    else:
+        pivot = arr[0]
+        less = [i for i in arr[1:] if i <= pivot]
+        greater = [i for i in arr[1:] if i > pivot]
+        return quick_sort(less) + [pivot] + quick_sort(greater)
+    
+print(quick_sort([98,77,33,1,8,4,99,4,10]))
